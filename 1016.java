98db6fd53ee08d14fab10e032fe6baa865841a65
yasmimgb <63204225+yasmimgb@users.noreply.github.com>
2020-05-27 20:45:38 -0300

    Distância

--- 1016.java
+++ 1016.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
 
+public class MyClass {
+    
+    public static void main(String[] args) {
+    
+    Scanner input = new Scanner(System.in);
+    
+    int distanciaFornecida = input.nextInt();
+    int qtdMinutos = (60 * distanciaFornecida) / 30;
+    
+    System.out.println(qtdMinutos + " minutos");
+    
+    input.close();
+    
+    }
+}
